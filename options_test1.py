79489bd4b95a26af2a93059ac327cf2ef2445f3f
Rohit02709 <164661162+Rohit02709@users.noreply.github.com>
2024-09-17 16:29:08 +0530

    Update options_test1.py

--- options_test1.py
+++ options_test1.py
@@ -72,7 +72,7 @@ try:
         # Sort by the absolute value of change in OI (largest change first)
         oi_sorted = oi.reindex(oi[['CALLS_Chng_in_OI', 'PUTS_Chng_in_OI']].abs().sum(axis=1).sort_values(ascending=False).index)
         # Select only the top 5 strikes with the most significant change in OI
-        oi_top_5 = oi_sorted.head(5)
+        oi_top_8 = oi_sorted.head(8)
         # Highlight the buy/sell signals and whether to buy CE or PE
         signal_table = oi_top_5[['CALLS_OI', 'CALLS_Chng_in_OI', 'PUTS_OI', 'PUTS_Chng_in_OI', 'Signal']].style.applymap(
             lambda val: 'color: green' if val == 'BUY CE' else 'color: red' if val == 'BUY PE' else 'color: black',
