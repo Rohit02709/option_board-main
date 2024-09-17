cebcf58c183648ebb275e066c6e4610849d39121
Rohit02709 <164661162+Rohit02709@users.noreply.github.com>
2024-09-17 16:44:57 +0530

    Update options_test1.py

--- options_test1.py
+++ options_test1.py
@@ -8,7 +8,18 @@ import pandas as pd
 import streamlit as st
 import time
 import streamlit as st
-
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 # add title of the web-app
 st.title(':red[NSE] **Option Dashboard**')
 st.header('Option Analysis', divider='rainbow')
